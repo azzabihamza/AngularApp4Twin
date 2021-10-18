e93a1478f903405b84bf8aa6453eb317bdfdde78
azzabihamza <hamza.azzabi@esprit.tn>
2021-10-18 11:33:45 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListuserComponent } from './listuser/listuser.component';
+import { MainProductComponent } from './main-product/main-product.component';
+import { MainProviderComponent } from './main-provider/main-provider.component';
+import { MainUserComponent } from './main-user/main-user.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"", redirectTo:"home",pathMatch:"full"},
+  {path:"mainUser", component: MainUserComponent, children:[
+    {path:"category/:cat", component: ListuserComponent},
+  ]},
+  {path:"mainproduct", component: MainProductComponent},
+  {path:"mainprovider", component: MainProviderComponent},
+  {path:"**", component: NotFoundPageComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListuserComponent } from './listuser/listuser.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+import { MainProductComponent } from './main-product/main-product.component';
+import { MainProviderComponent } from './main-provider/main-provider.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HomeComponent } from './home/home.component';
+import { MainUserComponent } from './main-user/main-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListuserComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainProductComponent,
+    MainProviderComponent,
+    NotFoundPageComponent,
+    HomeComponent,
+    MainUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
