9665e870100125223e66d8dcdc82ffc656c3b4e9
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-10 10:53:32 +0100

    implement service user

--- src/app/listuser/listuser.component.ts
+++ src/app/listuser/listuser.component.ts
@@ -1,6 +1,7 @@
   import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 
 @Component({
@@ -16,7 +17,8 @@ export class ListuserComponent implements OnInit {
   prop3 = 'test';
   searchval = '';
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private userService: UserService
     ) {}
   getVal(val: string) {
     this.prop1 = val;
@@ -26,63 +28,7 @@ export class ListuserComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => console.log(params.get('cat')));
 
-    this.list = [
-      {
-        idCustomer: 1,
-        firstName: 'Mila',
-        lastName: ' Kunis',
-        birthDate: '1999-06-30',
-        accountCategory: 'Admin',
-        email: 'mila@kunis.com',
-        password: 'test',
-        picture: 'https://bootdey.com/img/Content/avatar/avatar3.png',
-        profession: 'Software Engineer',
-      },
-      {
-        idCustomer: 2,
-        firstName: 'George',
-        lastName: 'Clooney',
-        birthDate: '1999-06-30',
-        accountCategory: 'Customer',
-        email: 'marlon@brando.com',
-        password: 'test',
-        picture: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-        profession: 'Software Engineer',
-      },
-      {
-        idCustomer: 3,
-        firstName: 'George',
-        lastName: 'Clooney',
-        birthDate: '1999-06-30',
-        accountCategory: 'Customer',
-        email: 'marlon@brando.com',
-        password: 'test',
-        picture: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-        profession: 'Software Engineer',
-      },
-      {
-        idCustomer: 4,
-        firstName: 'Ryan',
-        lastName: 'Gossling',
-        birthDate: '1999-06-30',
-        accountCategory: 'Golden',
-        email: 'Ryan@nicholson.com',
-        password: 'test',
-        picture: 'https://bootdey.com/img/Content/avatar/avatar4.png',
-        profession: 'Software Engineer',
-      },
-      {
-        idCustomer: 5,
-        firstName: 'Robert',
-        lastName: 'Downey',
-        birthDate: '1999-06-30',
-        accountCategory: 'Blocked Account',
-        email: 'robert@nicholson.com',
-        password: 'test',
-        picture: 'https://bootdey.com/img/Content/avatar/avatar5.png',
-        profession: 'Software Engineer',
-      },
-    ];
+    this.list = this.userService.getUsers();
 
     this.listInitiale=this.list;
      this.route.paramMap.subscribe(
